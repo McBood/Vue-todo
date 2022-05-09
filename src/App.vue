d44f5dcc4126c7c4ae49c212bebffda43d727c0e
Eugene Sukhovarov <mcboodvk@gmail.com>
2022-05-09 17:40:22 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h3>Today Tasks</h3>
+  <div class="tasks">
+    <div
+      class="task"
+      :class="{ completed: task.isCompleted }"
+      v-for="task in tasks"
+      :key="task.id"
+      @click="completedChange(task.id)"
+    >
+      <div
+        class="round"
+        :class="{
+          business: task.type === 'business',
+          personal: task.type === 'personal',
+        }"
+      ></div>
+      <span :class="{ completed: task.isCompleted }">{{ task.name }}</span>
+      <button @click="delTask(task.id)" class="btnDel">Delete</button>
+    </div>
+  </div>
+
+  <div class="add-task">
+    <input type="text" placeholder="Name of task" v-model="taskName" />
+    <button @click="addTask" class="btnAdd">Add</button>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      taskName: "",
+      tasks: [
+        {
+          id: "2132145123124",
+          name: "Create todo",
+          isCompleted: false,
+          type: "business",
+        },
+      ],
+    };
+  },
+  methods: {
+    completedChange: function (taskId) {
+      const currentTask = this.tasks.find((t) => t.id === taskId);
+      currentTask.isCompleted = !currentTask.isCompleted;
+    },
+    addTask: function () {
+      this.tasks.push({
+        id: Math.random().toString(36).substring(2, 7),
+        name: this.taskName,
+        type: "personalz",
+        isCompleted: false,
+      });
+      this.taskName = "";
+    },
+    delTask: function (taskId) {
+      this.tasks = this.tasks.filter((t) => t.id != taskId);
+    },
+  },
+};
+</script>
